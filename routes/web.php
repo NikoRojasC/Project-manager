b20e02029142c6b144a78b694d4ca9027be497c6
Nikolas Rojas <nikolasrojasc@gmail.com>
2024-05-10 22:53:02 +0200

    delete unnecesary function in the route file web.php

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 
     Route::resource('/projects', ProjectController::class);
-    Route::put('/projects/{project}', [ProjectController::class, 'update'])->name('projects.upd');
     Route::resource('/users', UserController::class);
     Route::resource('/tasks', TaskController::class);
 });
